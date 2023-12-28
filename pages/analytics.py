e4318e9de7cf203b72d6cd7c08c6c058c89ef64d
nonwor <nonwor@nonwors-MBP.attlocal.net>
2023-12-27 22:08:03 -0500

    making api call from dash with request

--- pages/analytics.py
+++ pages/analytics.py
@@ -1,35 +1,73 @@
 #Analytics.py
 
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import Dash, dcc, html, Input, Output, State, callback
+import requests
 
 dash.register_page(
-	__name__,
-	path='/analytics-dashboard',
-	title='Our analytics dashboard',
-	name='Our analytics dashboard'
+    __name__,
+    path='/analytics-dashboard',
+    title='Our analytics dashboard',
+    name='Our analytics dashboard'
 )
 
 layout = html.Div([
-	html.H1('This is our analytics page'),
-	html.Div([
-		"Select a city: ",
-		dcc.RadioItems(
-			options=["New York City", "Montreal","San Francisco"],
-			value="Montreal",
-			id='analytics-input'
-		)
+    html.H1('This is our analytics page'),
+    html.Div([
+        "Select a city: ",
+        dcc.RadioItems(
+            options=["New York City", "Montreal","San Francisco"],
+            value="Montreal",
+            id='analytics-input'
+        )
 
-	]),
-	html.Br(),
-	html.Div(id="analytics-output")
+    ]),
+    html.Br(),
+    html.Div(id="analytics-output"),
+    html.Br(),
+    html.Br(),
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit')
 ])
 
 @callback(
-	Output('analytics-output','children'),
-	Input('analytics-input','value')
+    Output('analytics-output','children'),
+    Input('analytics-input','value')
 )
 
 def update_city_selected(input_value):
-	
-	return f'you selected:{input_value}'
+
+    return f'you selected:{input_value}'
+
+
+@callback(
+    Output('container-button-basic', 'children'),
+    Input('submit-val', 'n_clicks'),
+    State('input-on-submit', 'value'),
+    prevent_initial_call=True
+)
+
+def get_api(n_clicks, value):
+    print(type(value),value)
+
+    api_endpoint = 'https://swapi.dev/api/people/'+value
+
+    try: 
+        response = requests.get(api_endpoint)
+
+        if response.status_code == 200:
+            data = response.json()
+            print(data['name'])
+            return(data['name'])
+        else:
+            print("API Error")
+            print(response.status_code)
+    except Exception as e:
+        print(e)
+
+    return 'The input value was "{}" and the button has been clicked {} times'.format(
+        value,
+        n_clicks
+    )
--- pages/home.py
+++ pages/home.py
@@ -5,6 +5,6 @@ from dash import html
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-	html.H1("This is our Home page"),
+	html.H1("Welcome to the home page!"),
 	html.Div('This is our home page content...'),
 ])
