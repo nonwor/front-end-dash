99341445471531c07b59086187f7a9f4eea8fbfc
nonwor <nonwor@nonwors-MBP.attlocal.net>
2024-01-10 11:34:01 -0500

    update analysis for two query input

--- pages/table.py
+++ pages/table.py
@@ -1,6 +1,6 @@
 
 import dash
-from dash import Dash, Input, Output, callback, dash_table
+from dash import Dash, Input, Output, callback, dash_table, html, dcc
 import pandas as pd
 
 
@@ -15,4 +15,17 @@ dash.register_page(
 
 df = pd.read_csv('http://127.0.0.1:5000/users/table')
 
-layout = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+layout = html.Div([
+    dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
+    html.Div(id='dd-output-container'),
+    dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+])
+# layout = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+
+@callback(
+    Output('dd-output-container', 'children'),
+    Input('demo-dropdown', 'value')
+)
+
+def update_output(value):
+    return f'You have selected {value}'
